719872c56e3afdc53ce5801535815d9f4a9f2bc8
alexsslemos <alexsslemos@gmail.com>
2024-06-10 19:44:00 -0300

    commit inicial

---
+++ src/main/java/br/com/alex/gravadora/GravadoraJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alex.gravadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravadoraJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravadoraJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/alex/gravadora/GravadoraJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alex.gravadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravadoraJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
